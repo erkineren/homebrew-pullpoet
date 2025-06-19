3dfa6a9500db00691891da32d5725e161fc6358e
erkineren <ernerkin@gmail.com>
2025-06-19 11:55:04 +0300

    Update pullpoet to v1.2.3

--- pullpoet.rb
+++ pullpoet.rb
@@ -1,8 +1,8 @@
 class Pullpoet < Formula
   desc "Generate AI-powered pull request descriptions by analyzing git diffs"
   homepage "https://github.com/erkineren/pullpoet"
-  url "https://github.com/erkineren/pullpoet/archive/v1.2.2.tar.gz"
-  sha256 "036def2c7bb1892c8edc7c2f096cd5b189c9aa5280cee1d258475a0da6aeae90"
+  url "https://github.com/erkineren/pullpoet/archive/v1.2.3.tar.gz"
+  sha256 "85043fe1489177bce2b7c0b2da00a3c15301d2e1cb1c493cccd7e7c2356acd26"
   license "MIT"
   head "https://github.com/erkineren/pullpoet.git", branch: "main"
 
